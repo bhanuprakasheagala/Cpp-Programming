72c5381099ec48d0528e3218405121e3a028bae7
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2023-11-20 20:29:24 +0530

    Function overloading without templates

--- Basics/FunctionTemplateExample.cpp
+++ Basics/FunctionTemplateExample.cpp
@@ -9,6 +9,18 @@ void print_array(T array) {
     std::cout<<std::endl;
 }
 
+/* C++20:
+In c++20 auto keyword can be used instead of template. g++ --std=c++20 <filename.cpp>
+The above code from line4 to line10 can be written:
+void print_array(auto array) {
+    for(auto value : array) {
+        std::cout << value << ' ';
+    }
+    std::cout<<std::endl;
+}
+*/
+
+
 int main() {
     std::array<int, 3> int_array = {1,2,3};
     // print_array<std::array<int, 3>>(int_array); OR

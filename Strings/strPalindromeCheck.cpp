aa9e18cce7f4ec0b90932df431e2af61ab73fb33
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2023-12-02 16:35:38 +0530

    Just another way

--- Strings/strPalindromeCheck.cpp
+++ Strings/strPalindromeCheck.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+/*
 std::string isPalindrome(std::string& input){
     int len = input.length();
     for(int i=0; i<(len/2); i++) {
@@ -10,10 +11,24 @@ std::string isPalindrome(std::string& input){
 
     return "Yes";
 }
+*/
+std::string isPalindrome(std::string& input) {
+    int l = 0;
+    int r = input.length() - 1;
+    while(l <= r){
+        if(input[l] != input[r]){
+            return "No";
+        }
+        l++;
+        r--;
+    }
+    return "Yes";
+}
+
 int main(){
     std::string input;
     std::cout<<"Enter string: "<<std::endl;
     std::getline(std::cin, input);
-    std::cout<<"Is"<<input<<"Palindrome? :"<<isPalindrome(input)<<std::endl;
+    std::cout<<"Is "<<input<<" Palindrome? : "<<isPalindrome(input)<<std::endl;
     return 0;
 }

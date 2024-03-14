217f2e88132c96f221e504bef85ad6928a0c3eb3
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-03-14 08:09:32 +0530

    Hash Table : Inprogress

--- STL/HashTableImplementation.cpp
+++ STL/HashTableImplementation.cpp
@@ -26,6 +26,31 @@ typedef struct HashTable {
     int count;
 } HashTable;
 
+// Create items by allocating memory for a key and value, and return a pointer to the item
+Ht_item* create_item(char* key, char* value) {
+    Ht_item* item = (Ht_item)malloc(sizeof(Ht_item));
+    item->key = (char*)malloc(strlen(key)+1);
+    item->value = (char*)malloc(strlen(value)+1);
+    strcpy(item->key, key);
+    strcpy(item->value, value);
+
+    return item;
+}
+
+// Create the table by allocating memory and setting size, count, and items
+HashTable* create_table(int size) {
+    HashTable* table = (HashTable*)malloc(sizeof(HashTable));
+    table->size = size;
+    table->count = 0;
+    table->items = (Ht_item**)calloc(table->size, sizeof(Ht_item*));
+
+    for(int i=0; i<table->size; ++i) {
+        table->items[i] = NULL;
+    }
+
+    return table;
+}
+
 int main()
 {
     

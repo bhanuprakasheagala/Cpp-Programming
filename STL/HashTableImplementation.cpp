480df52e5894f76c70eb668f8bf7504d4bd5c863
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-03-14 08:14:15 +0530

    Hash Table : Inprogress

--- STL/HashTableImplementation.cpp
+++ STL/HashTableImplementation.cpp
@@ -51,6 +51,26 @@ HashTable* create_table(int size) {
     return table;
 }
 
+// Write functions that free up a table item and the whole table
+void free_item(Ht_item* item) {
+    // Frees an item
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+void free_table(HashTable* table) {
+    // Frees the table
+    for(int i=0; i<table->size; ++i) {
+        Ht_item* item = table->items[i];
+        if(item != NULL) {
+            free_item(item);
+        }
+    }
+    free(table->items);
+    free(table);
+}
+
+
 int main()
 {
     

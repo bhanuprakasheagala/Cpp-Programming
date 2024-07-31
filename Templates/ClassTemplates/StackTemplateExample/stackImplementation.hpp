da6b73fb1d4acf48304d6f5980bb5abf50040c26
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-07-31 22:05:38 +0530

    Class template : Stack using vector

---
+++ Templates/ClassTemplates/StackTemplateExample/stackImplementation.hpp
@@ -0,0 +1,35 @@
+#include <cassert>
+#include <vector>
+
+template <typename T>
+class Stack {
+    private:
+        std::vector<T> elems;       // elements
+    
+    public:
+        void push(T const& elem);   // push elements
+        void pop();                 // pop elements
+        T const& top() const;       // return top elements
+        bool empty() const {
+            return elems.empty();   // return whether the stack is empty
+        }
+};
+
+template <typename T>
+void Stack<T>::push(T const& elem) {
+    elems.push_back(elem);          // append copy of passed elem
+}
+
+template <typename T>
+void Stack<T>::pop() {
+    assert(!elems.empty());
+    T elem = elems.back();
+    elems.pop_back();              // remove last element
+    return elem;
+}
+
+template <typename T>
+T const& Stack<T>::top() const {
+    assert(!elems.empty());
+    elems.back();
+}

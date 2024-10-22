ea0122cead8cc820b5d86a4d182ffc5d31798834
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-10-22 12:29:22 +0530

    Static variables and functions of a Class

--- 1.LanguageBasics/StaticKeyword/3.StaticMembersInClass.cpp
+++ 1.LanguageBasics/StaticKeyword/3.StaticMembersInClass.cpp
@@ -7,6 +7,10 @@ class Counter {
         Counter() {
             objectCount++;
         }
+
+        static void display() {
+            std::cout << "This is a static function\n";
+        }
 };
 
 int Counter::objectCount = 0; // Definition must be outside the class
@@ -15,12 +19,14 @@ int main()
 {
     Counter c1, c2, c3;
     std::cout << "Number of objects created: " << Counter::objectCount << '\n';
-    
+
+    Counter::display();
+
     return 0;
 }
 
 /*
 Notes:
 Static member variables are shared across all instances of the class. They are not tied to a specific object and can be accessed without creating an instance of the class.
-
+Static member functions can be called without creating an object of the class. They can only access static member variables and other static functions (not instance-specific members).
 */

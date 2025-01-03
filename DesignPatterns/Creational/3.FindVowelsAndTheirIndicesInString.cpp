b10d57e344bd7d02a3af827c7e4fbfb4e3ea2a92
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2025-01-03 16:44:23 +0530

    Find the vowels and their indices in a given string

---
+++ DesignPatterns/Creational/3.FindVowelsAndTheirIndicesInString.cpp
@@ -0,0 +1,79 @@
+/*
+Write a Singleton class to find vowels from a string and their indices
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class VowelFinder {
+private:
+    static VowelFinder* instance;
+    VowelFinder() {
+        std::cout << "Private constructor to prevent instantiation\n";
+    }
+    
+public:
+    static VowelFinder* getInstance() {
+        if(instance == nullptr) {
+            instance = new VowelFinder();
+        }
+        
+        return instance;
+    }
+    
+    void findVowelsAndIndexes(const std::string& str) {
+        std::vector<std::pair<char, int>> vowels;
+        
+        for(int i=0; i<str.length(); ++i) {
+            char c = tolower(str[i]);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vowels.push_back({str[i], i});
+            }
+        }
+        
+        if(!vowels.empty()) {
+            std::cout << "Vowels found in the string and their indices:\n";
+            for(auto& vowel : vowels) {
+                std::cout << "Vowel: " << vowel.first << ", Index: " << vowel.second << '\n'; 
+            }
+        }
+        else {
+            std::cout << "No vowels found in the string\n";
+        }
+    }
+};
+
+VowelFinder* VowelFinder::instance = nullptr;
+
+int main() {
+    
+    std::string str;
+    std::cout << "Enter a string:\n";
+    std::getline(std::cin, str);
+    
+    VowelFinder* vf = VowelFinder::getInstance();
+    vf->findVowelsAndIndexes(str);
+    
+    
+    return 0;
+}
+
+/*
+Output:
+Enter a string:
+Get the vowels and their indices
+Private constructor to prevent instantiation
+Vowels found in the string and their indices:
+Vowel: e, Index: 1
+Vowel: e, Index: 6
+Vowel: o, Index: 9
+Vowel: e, Index: 11
+Vowel: a, Index: 15
+Vowel: e, Index: 21
+Vowel: i, Index: 22
+Vowel: i, Index: 25
+Vowel: i, Index: 28
+Vowel: e, Index: 30
+*/
